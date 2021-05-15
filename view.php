a940a0017d920ae19f6f4f35b802b9b48f50dd3a
Benoît Cordier <bca.cordier@gmail.com>
2021-05-15 15:17:46 +0200

    Correction view

--- view.php
+++ view.php
@@ -12,7 +12,7 @@
 
     <div>
         <h2>Se connecter</h2>
-        <form method="POST" action="/Test/model_connection.php">
+        <form method="POST" action="model_connection.php">
             <label for="user_login">Login :</label>
             <input id="user_login" type="text" name="login" />
             <label for="user_login">Password :</label>
@@ -25,17 +25,17 @@
 
     <div>
         <h2>S'enregistrer</h2>
-        <form method="POST" action="/Test/model_inscription.php">
-            <label for="new_login">Login :</label>
-            <input id="new_login" type="text" name="new_login" />
-            <label for="new_firstname">Prénom :</label>
-            <input id="new_firstname" type="text" name="new_firstname" />
-            <label for="new_lastname">Nom :</label>
-            <input id="new_lastname" type="text" name="new_lastname" />
-            <label for="new_email">E-mail :</label>
-            <input id="new_email" type="text" name="new_email" />
-            <label for="new_password">Mot de passe :</label>
-            <input id="new_password" type="text" name="new_password" />
+        <form method="POST" action="model_inscription.php">
+            <label for="user_name">Login :</label>
+            <input id="user_name" type="text" name="user_name" />
+            <label for="first_name">Prénom :</label>
+            <input id="first_name" type="text" name="first_name" />
+            <label for="last_name">Nom :</label>
+            <input id="last_name" type="text" name="last_name" />
+            <label for="e_mail">E-mail :</label>
+            <input id="e_mail" type="text" name="e_mail" />
+            <label for="password">Mot de passe :</label>
+            <input id="password" type="text" name="password" />
             <label for="confirmation_password">Confirmez votre mot de passe :</label>
             <input id="confirmation_password" type="text" name="confirmation_password" />
             <input type="submit" value="S'enregistrer">
