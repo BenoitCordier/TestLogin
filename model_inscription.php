3f611f0396fe8fc0b392d35a90da0f38d8254381
Benoît Cordier <bca.cordier@gmail.com>
2021-05-15 15:17:23 +0200

    Correction finale de model_inscription.php

--- model_inscription.php
+++ model_inscription.php
@@ -3,30 +3,33 @@
 $user_name = !empty($_POST['user_name']) ? $_POST['user_name'] : NULL;
 $password = !empty($_POST['password']) ? $_POST['password'] : NULL;
 $e_mail = !empty($_POST['e_mail']) ? $_POST['e_mail'] : NULL;
+$first_name = !empty($_POST['first_name']) ? $_POST['first_name'] : NULL;
+$last_name = !empty($_POST['last_name']) ? $_POST['last_name'] : NULL;
 
-function inscription($user_name, $password, $e_mail) {
+function inscription($user_name, $password, $e_mail, $first_name, $last_name) {
 	
 	try
 	{
 		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+
+		$pass_hache = password_hash($password, PASSWORD_DEFAULT); 
+
+		$req = $bdd->prepare('INSERT INTO user(user_name, first_name, last_name, e_mail, password, function) VALUES(:user_name, :first_name, :last_name, :e_mail, :password, "user")');
+		$req->bindParam(':user_name', $user_name);
+		$req->bindParam(':first_name', $first_name);
+		$req->bindParam(':last_name', $last_name);
+		$req->bindParam(':e_mail', $e_mail);
+		$req->bindParam(':password', $pass_hache);
+		
+		$req->execute();
+
+		echo "Bienvenue " . $user_name . " ! Vous êtes enregistré !";
 	}
 	catch(Exception $e)
 	{
 		die('Erreur : '.$e->getMessage());
-	}
-
-	$first_name = !empty($_POST['first_name']) ? $_POST['first_name'] : NULL;
-	$last_name = !empty($_POST['last_name']) ? $_POST['last_name'] : NULL;
-
-	$pass_hache = !empty($_POST['password']) ? password_hash($_POST['password'], PASSWORD_DEFAULT) : NULL; 
-
-	$req = $bdd->prepare('INSERT INTO user(user_name, first_name, last_name, e_mail, password) VALUES(:user_name, :first_name, :last_name, :e_mail, :password');
-	$req->execute(array(
-		'user_name' => $user_name,
-		'first_name'  => $first_name,
-		'last_name' => $last_name,
-		'e_mail' => $e_mail,
-		'password' => $pass_hache));
+	}	
+	
 }
 
-inscription($user_name, $password, $e_mail);
+inscription($user_name, $password, $e_mail, $first_name, $last_name);
