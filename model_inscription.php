d0a5ee27c254d30738c06b5169d562fc8427ad6c
Benoît Cordier <bca.cordier@gmail.com>
2021-05-18 12:12:15 +0200

    model_inscription : ajout des conditions avant enregistrement bdd - pt2

--- model_inscription.php
+++ model_inscription.php
@@ -12,48 +12,67 @@ function inscription($user_name, $password, $e_mail, $first_name, $last_name, $c
 	try
 	{
 		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+		
+		$mysqli = new mysqli("localhost", "root", "root", "test");
 
-		if ($user_name === NULL)
+		if($mysqli->connect_error)
 		{
-			echo "Veuillez saisir un nom d'utilisateur valide.";
-		}
-		elseif($password === NULL)
-		{
-			echo "Veuillez saisir un mot de passe.";
-		}
-		elseif($confirmation_password !== $password || $confirmation_password === NULL)
-		{
-			echo "Veuillez confirmer votre mot de passe.";
-		}
-		elseif ($e_mail === NULL || !preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['e_mail']))
-		{
-			echo "Veuillez saisir une adresse e-mail valide.";
-		}
-		elseif ($first_name === NULL || $last_name === NULL)
-		{
-			echo "Veuillez indiquer vos noms et prénoms.";
+			die("Connection failed: " . $conn->connect_error);
 		}
 		else
 		{
-			$pass_hache = password_hash($password, PASSWORD_DEFAULT); 
+		$query_user = mysqli_query($mysqli, "SELECT * FROM user WHERE user_name='$user_name'");
+		$query_email = mysqli_query($mysqli, "SELECT * FROM user WHERE e_mail='$e_mail'");
 
-			$req = $bdd->prepare('INSERT INTO user(user_name, first_name, last_name, e_mail, password, function) VALUES(:user_name, :first_name, :last_name, :e_mail, :password, "user")');
-			$req->bindParam(':user_name', $user_name);
-			$req->bindParam(':first_name', $first_name);
-			$req->bindParam(':last_name', $last_name);
-			$req->bindParam(':e_mail', $e_mail);
-			$req->bindParam(':password', $pass_hache);
-			
-			$req->execute();
+			if(mysqli_num_rows($query_user) > 0)
+			{
+				echo "Ce nom d'utilisateur est déjà pris. Veuillez en choisir un autre.";   
+			}
+			elseif(mysqli_num_rows($query_email) > 0)
+			{
+				echo "Cet e-mail est déjà utilisé. Veuillez en indiquer un autre.";
+			}		
+			elseif ($user_name === NULL)
+			{
+				echo "Veuillez saisir un nom d'utilisateur valide.";
+			}
+			elseif($password === NULL)
+			{
+				echo "Veuillez saisir un mot de passe.";
+			}
+			elseif($confirmation_password !== $password || $confirmation_password === NULL)
+			{
+				echo "Veuillez confirmer votre mot de passe.";
+			}
+			elseif ($e_mail === NULL || !preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['e_mail']))
+			{
+				echo "Veuillez saisir une adresse e-mail valide.";
+			}
+			elseif ($first_name === NULL || $last_name === NULL)
+			{
+				echo "Veuillez indiquer vos noms et prénoms.";
+			}
+			else
+			{
+				$pass_hache = password_hash($password, PASSWORD_DEFAULT); 
 
-			echo "Bienvenue " . $first_name . " " . $last_name . " ! Vous vous êtes enregistré sous le pseudonyme " . $user_name . " !";
+				$req = $bdd->prepare('INSERT INTO user(user_name, first_name, last_name, e_mail, password, function) VALUES(:user_name, :first_name, :last_name, :e_mail, :password, "user")');
+				$req->bindParam(':user_name', $user_name);
+				$req->bindParam(':first_name', $first_name);
+				$req->bindParam(':last_name', $last_name);
+				$req->bindParam(':e_mail', $e_mail);
+				$req->bindParam(':password', $pass_hache);
+				
+				$req->execute();
+
+				echo "Bienvenue " . $first_name . " " . $last_name . " ! Vous vous êtes enregistré sous le pseudonyme " . $user_name . " !";
+			}
 		}
 	}
 	catch(Exception $e)
 	{
 		die('Erreur : '.$e->getMessage());
 	}	
-	
 }
 
 inscription($user_name, $password, $e_mail, $first_name, $last_name, $confirmation_password);
